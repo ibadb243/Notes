0445fe2eb975de58149f3d9dbf49c2b1e90e0c77
IbadDesktop <kronosxplay243@gmail.com>
2024-12-09 16:55:50 +0400

    Initials

---
+++ Notes.Application/Interfaces/INoteDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nodes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Interfaces
+{
+    public interface INoteDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nodes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Persistence.EntityTypeConfigurations
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(_ => _.Id);
+            builder.HasIndex(_ => _.Id).IsUnique();
+
+            builder.HasIndex(_ => _.UserId);
+
+            builder.Property(_ => _.Title).HasMaxLength(256);
+            builder.Property(_ => _.Details).HasMaxLength(4096);
+        }
+    }
+}
