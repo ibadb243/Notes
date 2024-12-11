82886c100113b00abc9110ec2a0dac95af7021a2
IbadDesktop <kronosxplay243@gmail.com>
2024-12-11 18:09:53 +0400

    Custom Exception Middleware Handler

---
+++ Notes.WebApi/Middleware/CustomExceptionHandlerMiddlerware.cs
@@ -0,0 +1,56 @@
+﻿using FluentValidation;
+using System.Text.Json;
+using System.Net;
+using Notes.Application.Common.Exceptions;
+
+namespace Notes.WebApi.Middleware
+{
+    public class CustomExceptionHandlerMiddlerware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionHandlerMiddlerware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = string.Empty;
+
+            switch (ex)
+            {
+                case ValidationException validationException:
+                    code = HttpStatusCode.BadRequest;
+                    result = JsonSerializer.Serialize(validationException.Errors);
+                    break;
+                case NotFoundException notFoundException:
+                    code = HttpStatusCode.NotFound;
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            if (result == string.Empty)
+            {
+                result = JsonSerializer.Serialize(new { error = ex.Message });
+            }
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ Notes.WebApi/Middleware/CustomExceptionHandlerMiddlerwareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.WebApi.Middleware
+{
+    public static class CustomExceptionHandlerMiddlerwareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionHandlerMiddlerware>();
+        }
+    }
+}
